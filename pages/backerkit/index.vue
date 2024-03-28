30f6e63ddc9c3ee50b7826085e9cd91edbad6979
David Ward <professorx00@gmail.com>
2024-03-27 22:04:57 -0400

    Added a page for the Backerkit redirect

---
+++ pages/backerkit/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <div class="hero min-h-screen" style="background-image: url('/imagination.jpg')">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-6xl">
+           
+            <NuxtLink class="text-4xl text-center  mt-3 border-2 rounded-lg p-3 bg-secondary_m hover:bg-accent_m hover:text-primary_m"  to="https://www.backerkit.com/call_to_action/92cd4630-e662-4b87-a010-771fb23030ed/landing" >Follow The Backerkit</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
