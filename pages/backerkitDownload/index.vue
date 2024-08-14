4e0ba2e9b07ead852dd07e1d127a7764487c24c7
David Ward <professorx00@gmail.com>
2024-08-14 07:02:56 -0400

    Fixed a QuickStartGuide issue

--- pages/backerkitDownload/index.vue
+++ pages/backerkitDownload/index.vue
@@ -2,7 +2,7 @@
     <div class="w-full flex flex-col items-center justify-center">
         <div class="md:w-11/12 w-full relative flex flex-col items-center justify-center">
                 <img src="/download.png" alt="Quick Start Guide" class="lg:w-1/2 w-full m-4"/>
-                <a href="/QuickGuide_v3.pdf" class="lg:text-3xl md:text-2xl text-lg btn btn-primary_m m-2">Download Your Quick Guide Here</a>
+                <a href="/QuickGuide.pdf" class="lg:text-3xl md:text-2xl text-lg btn btn-primary_m m-2">Download Your Quick Guide Here</a>
                 </div>
     </div>
 </template>
